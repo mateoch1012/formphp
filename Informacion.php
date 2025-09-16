0a2c3dd957f9bfb4f6177ecd22f9344fbb42354b
mateoch1012 <mateoch1012@gmail.com>
2025-09-16 23:15:54 +0200

    Informacion.php

--- Informacion.php
+++ Informacion.php
@@ -1,10 +1,21 @@
 <?php
-$Identificacion= $_POST [ 'Identificacion'];
-$NomApellidos=$_POST ['NomApellidos'];
-$Cargo=$_POST [ 'Cargo'];
-$Salario=$_POST ['Salario');
-echo "La identificacion del Empleado es: ". Identificacion;
-echo "Los nombres y apellidos del empleado son: ". NomApellidos;
-echo "El cargo del empleado es: ".Cargo;
-echo "El salario del empleado es: ".Salario;
+// Captura de los datos enviados desde el formulario
+$nombre = $_POST['nombre'];
+$curso = $_POST['curso'];
+$numero = $_POST['numero'];
+
+// Calcular el cubo
+$cubo = pow($numero, 3);
+
+// Mostrar resultados
+echo "<h2>Resultados</h2>";
+echo "Nombre: " . $nombre . "<br>";
+echo "Curso de formación: " . $curso . "<br>";
+echo "El cubo del número $numero es: " . $cubo . "<br>";
+
+// Verificación del mensaje especial
+if ($cubo > 100) {
+    echo "<p><b>Eres muy afortunado</b></p>";
+}
 ?>
+
